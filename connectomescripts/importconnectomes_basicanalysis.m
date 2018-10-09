789900c02809649361914d68ca85ef72365c97d0
Perry <alistairperry123@gmail.com>
2018-10-09 16:00:11 +0200

    finalize

--- connectomescripts/importconnectomes_basicanalysis.m
+++ connectomescripts/importconnectomes_basicanalysis.m
@@ -1,11 +1,32 @@
-function importconnectomes_basicanalysis(parcnum, parcname, thr, sparsity, varargin)
+function importconnectomes_basicanalysis(parcnum, thr, sparsity, varargin)
 %Import raw connectomes and basic network analyses
 %Using new functions available within latest MRtrix package
 %Alistair Perry, UNSW (2014)
 
+%% Calling the function
+
 %Input arguments:
 %parcnum = Number of parcellation regions
-%parcname = 
+%thr = [0 or 1]: 0, no thresholding; 1, threshold matrices
+%sparsity = Threshold level - for e.g. 10 for 0.10 percent
+
+%Optional arguments:
+%varagin = normtype - if empty, by default will use the non-normalized
+%matrices for further analyses
+  
+    % Options: 
+        %ORGinv (normalized by track length), 
+        %ORGinvnodelength (normalized by both track length and node vol)
+
+%N.B: Must call function from directory containing subject folders 
+
+%Dependencies:
+%Brain Connectivity Toolbox - https://sites.google.com/site/bctnet/ 
+%Community Algorithm Toolbox - https://github.com/CarloNicolini/communityalg
+    %For communicability calculations only
+
+%% Batch Setup
+% First find subject folders in the working directory
 
 workingdirectory = pwd;
 files = dir(workingdirectory);
@@ -13,126 +34,123 @@ dirFlags=[files.isdir];
 subFolders=files(dirFlags);
 subFolders(1:2)=[];
 
+%% Run loop
+
 for s = 1:length(subFolders)
+    
+% Extract current subject informatiom    
     currentSubj= subFolders(s,1).name;
     currentSubjDir = char([workingdirectory '/' currentSubj]);
     
-    %setup output directory
+% Setup output directory
     
     outdirname=int2str(sparsity);
     mkdir([currentSubjDir '/' outdirname]);
-    
+ 
+% Catch whether to perform thresolding of matrix    
     if thr==1
-    
-    countfile = dir(fullfile(currentSubjDir, '*count*'));
+
+        %Pick up raw matrices stored as .csv files
+        
+        %Unfiltered track weights
+    countfile = dir(fullfile(currentSubjDir, '*streamweights*'));
     countmtx = dlmread([currentSubjDir '/' countfile.name], '%f', 0, 0, [0 0 (parcnum-1) (parcnum-1)]);
     
-    invcountfile = dir(fullfile(currentSubjDir, '*invlengths*'));
+        %Normalized by fiber length
+    invcountfile = dir(fullfile(currentSubjDir, '*invlengthweights'));
     invcountmtx = dlmread([currentSubjDir '/' invcountfile.name], '%f', 0, 0, [0 0 (parcnum-1) (parcnum-1)]);
     
-%     invnodelengthfile = dir(fullfile(currentSubjDir, '*invnodeandlengths*'));
-%     invnodelengthmtx = dlmread([currentSubjDir '/' invnodelengthfile.name]);
+        %Normalized by both node volume and length
+    invnodelengthfile = dir(fullfile(currentSubjDir, '*invnodelengthweights*'));
+    invnodelengthmtx = dlmread([currentSubjDir '/' invnodelengthfile.name]);
     
-    lengthfile = dir(fullfile(currentSubjDir, '*tracklengths*'));
-    lengthmtx = dlmread([currentSubjDir '/' lengthfile.name], '%f', 0, 0, [0 0 (parcnum-1) (parcnum-1)]);
+        %Remove for now
+    %lengthfile = dir(fullfile(currentSubjDir, '*tracklengths*'));
+    %lengthmtx = dlmread([currentSubjDir '/' lengthfile.name], '%f', 0, 0, [0 0 (parcnum-1) (parcnum-1)]);
     
     %parcload = load_untouch_nii([commondir '/' currentSubj '/' parcname '.nii']); 
     
+ % Symmetrize matrices 
     for i = 1:parcnum
         for j = i+1:parcnum
             if i == parcnum break; end
         countmtx(j,i) = countmtx(i,j);
-        lengthmtx(j,i) = lengthmtx(i,j);
+        %lengthmtx(j,i) = lengthmtx(i,j);
         invcountmtx(j,i)=invcountmtx(i,j);
-%         invnodelengthmtx(j,i)=invnodelengthmtx(i,j);
+        invnodelengthmtx(j,i)=invnodelengthmtx(i,j);
         end
     end
-    
+ 
+ % Setup subject structure    
     SubjStruct=struct;
-    
+
+ % Load in the raw matrices   
     SubjStruct.ORG=countmtx;
     SubjStruct.ORGinv=invcountmtx;
-%     SubjStruct.ORGinvnodelength=invnodelengthmtx;
-    SubjStruct.tckdistmat=lengthmtx;
+    SubjStruct.ORGinvnodelength=invnodelengthmtx;
+    %SubjStruct.tckdistmat=lengthmtx;
     
     %BrainMask = load_untouch_nii([currentSubjDir '/' 'biasb0brain_mask.nii']);
     %SubjStruct.BrainSize = numel(find(BrainMask.img~=0));
-    
-    [rois, ~] = extract_roi([currentSubjDir '/' parcname '' '.nii']);
-    SubjStruct.distmat = zeros(parcnum, parcnum);
-        
-        for i = 1:parcnum
-            for j = 1:parcnum
-                SubjStruct.distmat(j, i) = sqrt(abs([(rois(i,1)-rois(j,1))*(rois(i,1)-rois(j,1))]+[(rois(i,2)-rois(j,2))*(rois(i,2)-rois(j,2))]+[(rois(i,3)-rois(j,3))*(rois(i,3)-rois(j,3))]));
-            end
-        end
-        
+ 
+ % Threshold the networks to desired sparsity    
     if ~isempty(varargin)
+        %catches the chosen normalization of the connectomes
 
     normtype=varargin{1};   
     
     SubjStruct.thr = threshold_proportional(SubjStruct.(normtype), (sparsity./100));
         
     else
+        %this assumes that you intend to process the non-normalized
+        %connectomes
         
-    SubjStruct.thr = threshold_proportional(SubjStruct.ORGinv, (sparsity./100));
+    SubjStruct.thr = threshold_proportional(SubjStruct.ORG, (sparsity./100));
     
     end
-    
+ 
+ % Binarize   
     SubjStruct.CIJ = weight_conversion(SubjStruct.thr, 'binarize');   
     
-    save([currentSubjDir '/' outdirname '/' currentSubj '' 'metrics.mat'], 'SubjStruct');
-    
-    end
-    
+    else
+%% Network computations start here
+% Can skip the thresholding steps, if already performed.
+
     load([currentSubjDir '/' outdirname '/' currentSubj  '' 'metrics.mat']);
     
-    %Distance info
-    SubjStruct.thrdistmat = zeros(parcnum,parcnum);
-    for i = 1:parcnum
-        for j = 1:parcnum
-            if SubjStruct.CIJ(i,j) == 1
-                SubjStruct.thrdistmat(i,j) = SubjStruct.tckdistmat(i,j);
-                SubjStruct.thrdistmat(j,i) = SubjStruct.tckdistmat(j,i);
-            end
-        end
-    end    
-    
-    SubjStruct.numfibers = sum(sum(SubjStruct.ORGinv));
+% Number of fibers within matrix    
+    SubjStruct.numfibers = sum(sum(SubjStruct.ORG));
        
-    %Basic nodal connectivity info
+ %Basic nodal connectivity calculatins
     SubjStruct.DEG = degrees_und(SubjStruct.CIJ);
     SubjStruct.STR = strengths_und(SubjStruct.thr);
     SubjStruct.BETC = betweenness_bin(SubjStruct.thr);
     
-    %Eff
-    CIJd = distance_bin(SubjStruct.CIJ);
-    SubjStruct.CPL = charpath(CIJd);
-    SubjStruct.EFF = efficiency_bin(SubjStruct.CIJ);
-    
-    %Local efficiency
+%Local efficiency
     SubjStruct.NodalEff=efficiency_bin(SubjStruct.CIJ,1);
     SubjStruct.NodalEff(isinf(SubjStruct.NodalEff))=0;
     
-    %Clustering
+%Global Efficiency
+    CIJd = distance_bin(SubjStruct.CIJ);
+    SubjStruct.CPL = charpath(CIJd);
+    SubjStruct.EFF = efficiency_bin(SubjStruct.CIJ);
+
+%Clustering
     SubjStruct.CCOEFF = clustering_coef_bu(SubjStruct.CIJ);
     SubjStruct.avgCCOEFF = mean(SubjStruct.CCOEFF);
     
-    %Communicability
-    
-    COMM = communicability_bu(SubjStruct.CIJ);
+%Communicability
+    COMM = communicability_wu(SubjStruct.CIJ);
     SubjStruct.TCOMM = sum(sum(triu(COMM)));
-    
-    %Distance Calcs
-    numbercon = nnz(SubjStruct.CIJ);
-    SubjStruct.totalDists = sum(sum(SubjStruct.thrdistmat));
-    SubjStruct.MAD = SubjStruct.totalDists./numbercon;
      
     fprintf('\n %s completed \n' , currentSubj);
     
     %Save output
     save([currentSubjDir '/' outdirname '/' currentSubj '' 'metrics.mat'], 'SubjStruct');
+
+    end
 end
+
 end
 
 
